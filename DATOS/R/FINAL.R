c018287db8971fc0bcf20d9bc468fc48646dec99
nicoleleppe12 <nicoleleppe12@gmail.com>
2021-08-27 02:15:55 -0400

    final

--- DATOS/R/FINAL.R
+++ DATOS/R/FINAL.R
@@ -1,2 +1,4 @@
 final= df_sem20
 final= rename(final,c(tasa_max_contagios="tasa_max", DENSIDAD="densidad",P_POB="p_pob", sem_curentena="sem_cuar",P_HACINAMIENTO="p_hacina",P_HACINAMIENTO_C="p_hacina_c", var_salidas="IVS"))
+corr_final = cor(final[,4:10])
+corrplot(corr_final,method = "pie", type = "upper", tl.cex = 0.7, number.cex = 0.6)

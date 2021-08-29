6938806ca29779175a50b1dc1e4432ab242b9516
nicoleleppe12 <nicoleleppe12@gmail.com>
2021-08-29 18:22:12 -0400

    asdkjsah

--- DATOS/R/INDICE_MORAN.R
+++ DATOS/R/INDICE_MORAN.R
@@ -8,15 +8,12 @@ shape@data = join(shape@data,df_sem20, by = "COD_COM")
 W = poly2nb(shape)
 W_list = nb2listw(W)
 # regresion a utilizar
-r1<- lm(tasa_max ~ sem_cuar + p_hacina_c + IVS , data = final)
-
-r3<- lm(tasa_max ~ sem_cuar + densidad + IVS , data = final)
-
-r4 = lm(tasa_max ~ log(sem_cuar) + p_hacina_c + IVS , data = final)
+modelo1<- lm(tasa_max ~ sem_cuar + p_hacina_c + IVS , data = final)
+modelo2<- lm(tasa_max ~ sem_cuar + densidad + IVS , data = final)
+modelo3 = lm(tasa_max ~ log(sem_cuar) + p_hacina_c + IVS , data = final)
 
 ### indice de moran 
-indice_moran_r1<-lm.morantest(r1, W_list)
-
-indice_moran_r3<-lm.morantest(r3, W_list)
-indice_moran_r4<-lm.morantest(r4, W_list)
+indice_moran_r1<-lm.morantest(modelo1, W_list)
+indice_moran_r3<-lm.morantest(modelo2, W_list)
+indice_moran_r4<-lm.morantest(modelo3, W_list)
 

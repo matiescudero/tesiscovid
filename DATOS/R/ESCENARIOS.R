976a16a8e18557e013530122066b4bf0313570f5
matiescudero <matias.escudero.b@usach.cl>
2021-08-29 19:17:07 -0400

    nuevo escenario

--- DATOS/R/ESCENARIOS.R
+++ DATOS/R/ESCENARIOS.R
@@ -13,3 +13,4 @@ df_final$tasa_est = -220.781 + 7.130*df_final$sem_cuar + 36.882*df_final$p_hacin
 df_final$sem13 = 13
 
 #tasa_max si todas las comunas hubiesen ingresado a cuarentena en la semana 13
+df_final$tasa_sem13 = -220.781 + 7.130*df_final$sem13 + 36.882*df_final$p_hacina_c + 8.368*df_final$IVS

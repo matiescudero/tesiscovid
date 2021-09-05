9b64232c1f2cb07364c3299a54bf2b687c4eb1c1
nicoleleppe12 <nicoleleppe12@gmail.com>
2021-09-04 20:20:21 -0400

    indice_moran

--- DATOS/R/INDICE_MORAN.R
+++ DATOS/R/INDICE_MORAN.R
@@ -19,21 +19,22 @@ W_list = nb2listw(W)
 # regresion a utilizar
 
 
-modelo1 <- lm(log(tasa_max) ~ sem_cuar + p_hacina_c + IVS , data = shape@data)
+modelo1 <- lm(tasa_max ~ sem_cuar + p_hacina_c + IVS , data = shape@data)
 modelo2 <- lm(tasa_max ~ sem_cuar + densidad + IVS , data = shape@data)
-modelo3 = lm(tasa_max ~ log(sem_cuar) + p_hacina_c + IVS , data = shape@data)
+modelo3 <- lm(tasa_max ~ log(sem_cuar) + p_hacina_c + IVS , data = shape@data)
+
 
 ### indice de moran 
 indice_moran_r1 <- lm.morantest(modelo1, W_list)
 indice_moran_r2<- lm.morantest(modelo2, W_list)
 indice_moran_r3 <- lm.morantest(modelo3, W_list)
 
+
 #Regresiones SAR
-sar_modelo1 = lagsarlm(formula =log(tasa_max) ~ sem_cuar + p_hacina_c + IVS , data = shape@data, listw = W_list)
+sar_modelo1 = lagsarlm(formula =tasa_max ~ sem_cuar + p_hacina_c + IVS , data = shape@data, listw = W_list)
 sar_modelo2 = lagsarlm(formula = tasa_max ~ sem_cuar + densidad + IVS , data = shape@data, listw = W_list)
 sar_modelo3 = lagsarlm(formula = tasa_max ~ log(sem_cuar) + p_hacina_c + IVS , data = shape@data, listw = W_list)
 
-sar_modelo4 = lagsarlm(formula = log(tasa_max) ~ sem_cuar + densidad + IVS , data = shape@data, listw = W_list)
 
 
 ####PRUEBA CON MATRICES####
